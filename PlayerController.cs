7b689da84de6e7de7629a34a98c138533e4fe906
SowadK25 <64669044+SowadK25@users.noreply.github.com>
2020-06-24 23:07:13 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))] // Always needs the player motor script
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 12f;
+    public PlayerMotor playerMotor;
+    public float sensitivity = 3f;
+
+    void Start()
+    {
+        playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Calculate movement as a 3D vector
+        var xMovement = Input.GetAxisRaw("Horizontal");
+        var zMovement = Input.GetAxisRaw("Vertical");
+
+        var movHorizontal = transform.right * xMovement;
+        var movVertical = transform.forward * zMovement;
+        
+        // Final movement vector
+        var velocity = (movHorizontal + movVertical).normalized * speed;
+        
+        // Apply movement to playerMotor script
+        playerMotor.Movement(velocity);
+        
+        // Calculate rotation for turning around
+        var yRotation = Input.GetAxisRaw("Mouse X");
+        var rotation = new Vector3(0f, yRotation, 0f) * sensitivity;
+        var xRotation = Input.GetAxisRaw("Mouse Y");
+        var cameraRotation = new Vector3(xRotation, 0f, 0f) * sensitivity;
+        
+        //Apply rotation
+        playerMotor.Rotation(rotation);
+        playerMotor.CameraRotation(cameraRotation);
+    }
+}
---
+++ PlayerMotor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))] // Always needs a rigid body
+public class PlayerMotor : MonoBehaviour
+{
+  public Vector3 velocity;
+  public Vector3 rotation;
+  public Vector3 cameraRotation;
+  public Rigidbody rb;
+  public Camera cam;
+
+  public Animator animator;
+  private static readonly int Running = Animator.StringToHash("Running");
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  public void Movement(Vector3 vel)
+  {
+    velocity = vel;
+  }
+
+  void PerformMovement()
+  {
+    if (velocity != Vector3.zero)
+    {
+      animator.SetBool(Running, true);
+      rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime); // Moves player
+    }
+    else if (velocity == Vector3.zero)
+    {
+      animator.SetBool(Running, false);
+    }
+  }
+
+  public void Rotation(Vector3 rot)
+  {
+    rotation = rot;
+  }
+
+  public void CameraRotation(Vector3 camRot)
+  {
+    cameraRotation = camRot;
+  }
+
+  void PerformRotation()
+  {
+    rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+    if (cam != null)
+    {
+      cam.transform.Rotate(-cameraRotation);
+    }
+    
+  }
+  private void FixedUpdate()
+  {
+    PerformMovement();
+    PerformRotation();
+  }
+}
